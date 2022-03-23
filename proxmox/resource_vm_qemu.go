85cc6fa61662b110397f176aa3e9f313ac29d2ed
Elsvent <elsvent@ghtinc.com>
2022-03-24 01:35:45 +0800

    asdff

--- proxmox/resource_vm_qemu.go
+++ proxmox/resource_vm_qemu.go
@@ -900,7 +900,7 @@ func resourceVmQemuCreate(d *schema.ResourceData, meta interface{}) error {
 		} else if d.Get("pxe").(bool) {
 			var found bool
 			bs := d.Get("boot").(string)
-			regs := [...]string{"^n.*$", "^order=net.*$", "^order=disk.*"}
+			regs := [...]string{"^n.*$", "^order=net.*$", "^order=scsi.*"}
 
 			for _, reg := range regs {
 				re, err := regexp.Compile(reg)
